503ef472907ce0769126b9f7f12370ded15aad37
c0493825 <antproenca92@gmail.com>
2018-10-22 01:36:38 -0300

    Estrutura básica, css, logo, rodapé, main, footer, menu

---
+++ src/components/home/home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Início"
+        subtitle="Gerenciamento de clientes">
+        <div className='display-4'>Bem vindo!</div>
+        <hr />
+        <p className="mb-0">Sistema para cadastro de clientes</p>
+    </Main>
--- src/components/template/Logo.jsx
+++ src/components/template/Logo.jsx
@@ -1,8 +1,11 @@
 import './Logo.css'
+import logo from '../../assets/imgs/brave-logo.png'
 import React from 'react'
 
 export default props => 
     <aside className="logo">
-        Logo
+        <a href="/" className="logo">
+            <img src={logo} alt="logo" />
+        </a>
     </aside>
 
